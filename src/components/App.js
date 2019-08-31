e318d34e3cdee529f3ac84edae3d6bd6e13f7164
Oliver <olla@html24.net>
2019-08-31 09:51:13 +0200

    Separates ImageList into separate component & destructures data from the unsplash call

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends Component {
 
@@ -8,7 +9,9 @@ class App extends Component {
 
     onSearchSubmit = async (term) => {
         const response = await unsplash.get('https://api.unsplash.com/search/photos', {
-            params: { query: term }
+            params: { 
+                query: term 
+            }
         });
 
         this.setState({ images: response.data.results });
@@ -18,7 +21,7 @@ class App extends Component {
         return (
         <div className="ui container" style={{marginTop: '10px'}}> 
             <SearchBar onSubmit={this.onSearchSubmit} />
-            Found {this.state.images.length} images
+            <ImageList images={this.state.images}/>
         </div>
         )
     }
