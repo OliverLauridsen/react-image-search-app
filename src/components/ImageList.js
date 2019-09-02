eff0c3b01790fb054b2f9bbb8d373d6b51df794f
Oliver <olla@html24.net>
2019-09-02 17:29:05 +0200

    Adds ImageCart component, that dynamically meassures span height and allocates space dependant on this. This is handled using CSS grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './imageList.css';
+import ImageCart from './ImageCart';
 
 const ImageList = (props) => {
-    const images = props.images.map(({description, id, urls}) => {
-        return <img alt={description} key={id} src={urls.regular} />
+    const images = props.images.map(image => {
+        return <ImageCart key={image.id} image={image} />
     });
 
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
